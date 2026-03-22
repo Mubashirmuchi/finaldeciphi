40bbe1a4e060b7c05551ee4411ea470645c8b287
Mubashir <mubashirmuchi75@gmail.com>
2026-03-23 00:42:20 +0530

    first commit

---
+++ next.config.ts
@@ -0,0 +1,32 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    ...(process.env.NODE_ENV === 'development' && {
+      unoptimized: true,
+    }),
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.strapi.io',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.strapiapp.com',
+      },
+    ],
+  },
+}
+
+export default nextConfig
