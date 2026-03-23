62341a1b6f4549c72e207c67a4227a9989d02ee3
Mubashir <mubashirmuchi75@gmail.com>
2026-03-23 13:10:06 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,33 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true,
   images: {
-    formats: ['image/avif', 'image/webp'],
-    ...(process.env.NODE_ENV === 'development' && {
+    formats: ["image/avif", "image/webp"],
+    ...(process.env.NODE_ENV === "development" && {
       unoptimized: true,
     }),
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
-        pathname: '/uploads/**',
+        protocol: "http",
+        hostname: "43.204.19.133",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+
+      {
+        protocol: "https",
+        hostname: "*.strapi.io",
+        pathname: "/uploads/**",
       },
       {
-        protocol: 'https',
-        hostname: '*.strapi.io',
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.strapiapp.com',
+        protocol: "https",
+        hostname: "*.strapiapp.com",
       },
     ],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
